10e49906382cb3387bbaba1580da3b28061503f7
Arsen3433rf <arsen3433rf@example.com>
2025-10-14 11:00:13 +0600

    Update server.js port and logic

--- server.js
+++ server.js
@@ -677,5 +677,5 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
