f961a09dc2cae8c2df87dedf39f870c503d1fad4
Arsen3433rf <arsen3433rf@example.com>
2025-10-16 12:04:07 +0600

    🔧 Fix: ensure PostgreSQL connection

--- server.js
+++ server.js
@@ -7,13 +7,8 @@ const { Pool } = require('pg');
 
 const app = express();
 
-const db = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  port: process.env.PGPORT,
-  ssl: { rejectUnauthorized: false }
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
 });
 
 db.connect()
