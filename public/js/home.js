44b58a3c0f95e8bb253276aa11be0d1294db7d3a
Arsen3433rf <arsen3433rf@example.com>
2025-10-28 16:40:07 +0600

    лавная страница ERP: табы с возвратом текста, наполненный контент

--- public/js/home.js
+++ public/js/home.js
@@ -11,3 +11,23 @@ document.querySelectorAll('.tab-btn').forEach(btn => {
     }
   });
 });
+const buttons = document.querySelectorAll('.tab-btn');
+const sections = document.querySelectorAll('.tab-content');
+const defaultText = document.getElementById('default-text');
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const targetId = btn.getAttribute('data-target');
+    const target = document.getElementById(targetId);
+
+    if (target.classList.contains('show')) {
+      target.classList.remove('show');
+      defaultText.style.display = 'block';
+    } else {
+      sections.forEach(s => s.classList.remove('show'));
+      target.classList.add('show');
+      defaultText.style.display = 'none';
+    }
+  });
+});
+
